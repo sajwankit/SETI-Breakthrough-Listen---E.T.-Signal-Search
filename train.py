ee8f466d8441c17cd3459eb4bbf961e75cda3b74
sajwankit <sajwanankit6@gmail.com>
2021-05-22 23:38:42 +0000

    removing useless code pieces

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import argparse
 
 import albumentations
 import torch
-import torchsample
 
 from sklearn import metrics
 
@@ -15,44 +14,6 @@ import engine
 import models
 import validation_strategy as vs
 
-from sklearn.model_selection import StratifiedKFold
-
-class StratifiedSampler(torchsample.Sampler):
-    """Stratified Sampling
-    Provides equal representation of target classes in each batch
-    """
-    def __init__(self, class_vector, batch_size):
-        """
-        Arguments
-        ---------
-        class_vector : torch tensor
-            a vector of class labels
-        batch_size : integer
-            batch_size
-        """
-        self.n_splits = int(class_vector.size(0) / batch_size)
-        self.class_vector = class_vector
-
-    def gen_sample_array(self):
-        try:
-            from sklearn.model_selection import StratifiedShuffleSplit
-        except:
-            print('Need scikit-learn for this functionality')
-        import numpy as np
-        
-        s = StratifiedShuffleSplit(n_splits=self.n_splits, test_size=0.5)
-        X = torch.randn(self.class_vector.size(0),2).numpy()
-        y = self.class_vector.numpy()
-        s.get_n_splits(X, y)
-
-        train_index, test_index = next(s.split(X, y))
-        return np.hstack([train_index, test_index])
-
-    def __iter__(self):
-        return iter(self.gen_sample_array())
-
-    def __len__(self):
-        return len(self.class_vector)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -70,7 +31,7 @@ if __name__ == '__main__':
     target_size = config.TARGET_SIZE
 
     df = pd.read_csv(data_path+'train_labels.csv')
-    df = pd.concat([df.query('target == 1'), df.query('target == 0').sample(len(df.query('target == 0'))//7)]).sample(frac=1).reset_index(drop=True)
+    df = pd.concat([df.query('target == 1'), df.query('target == 0').sample(len(df.query('target == 0'))//10)]).sample(frac=1).reset_index(drop=True)
     images = list(glob.glob(data_path+'train/*'))
     targets = df.target.values
 
@@ -100,12 +61,6 @@ if __name__ == '__main__':
                                                 resize = None,
                                                 augmentations = None)
 
-        # train_loader = torch.utils.data.DataLoader(train_dataset,
-        #                                                 sampler=StratifiedSampler(class_vector = torch.from_numpy(np.array(train_targets)),
-        #                                                                                  batch_size = bs),
-        #                                                 batch_size = bs
-        #                                                 # batch_sampler = StratifiedBatchSampler(y = np.array(train_targets), batch_size = bs),
-        #                                             )
         train_loader = torch.utils.data.DataLoader(train_dataset,
                                             batch_size = bs,
                                             shuffle = True,
