c50b0b88225e5061b22c2042f787c3a42935a61b
sajwankit <sajwanankit6@gmail.com>
2021-05-29 11:30:16 +0000

    design image code

--- designimg.py
+++ designimg.py
@@ -5,7 +5,7 @@ import glob
 import cv2
 
 class DesignImage():
-    def __init__(self, images_set, out_image_size = (224, 224), chl_pos_in_spatial = [2, 1, 0, 3, 4, 5]):
+    def __init__(self, images_set, out_image_size = (224, 224), chl_pos_in_spatial):
         self.images_set = images_set
         self.image_paths = list(glob.glob(f'{config.DATA_PATH}{self.images_set}/*/*.npy'))
         # p{channel} means position of channel in the final spatial image,  p{channel}: -1 means a channel is excluded
@@ -21,29 +21,33 @@ class DesignImage():
         # 0 1         .
         # 2 3         .
         # 4 5       256*3
-        for pos_idx in range(0, image_array.shape[0]):
-            if pos_idx % 2 == 0:
-                channel = self.chl_pos_in_spatial.index(pos_idx)
-                channel_image = image_array[channel,:,:]
+        # looping over pos_idx values to get corresponding channel 
+        for ch_pos_idx in range(0, len(self.chl_pos_in_spatial)):
+            if ch_pos_idx % 2 == 0:
+                try:
+                    channel = self.chl_pos_in_spatial.index(ch_pos_idx)
+                    channel_image = image_array[channel,:,:]
+                except:
+                    channel_image = 0
                 x_start = 0
                 x_end = image_array.shape[1]
-                y_start = int(pos_idx*0.5) * image_array.shape[2]
-                y_end = int((pos_idx*0.5)+1) * image_array.shape[2]
+                y_start = int(ch_pos_idx*0.5) * image_array.shape[2]
+                y_end = int((ch_pos_idx*0.5)+1) * image_array.shape[2]
                 image_array_spatial[x_start: x_end, y_start: y_end] = channel_image
             else:
-                channel = self.chl_pos_in_spatial.index(pos_idx)
-                channel_image = image_array[channel,:,:]
+                try:
+                    channel = self.chl_pos_in_spatial.index(ch_pos_idx)
+                    channel_image = image_array[channel,:,:]
+                except:
+                    channel_image = 0
                 x_start = image_array.shape[1]
                 x_end = 2 * image_array.shape[1]
-                y_start = int((pos_idx-1)*0.5)*image_array.shape[2]
-                y_end = int((pos_idx-1)*0.5+1)*image_array.shape[2]
+                y_start = int((ch_pos_idx-1)*0.5)*image_array.shape[2]
+                y_end = int((ch_pos_idx-1)*0.5+1)*image_array.shape[2]
                 image_array_spatial[x_start: x_end, y_start: y_end] = channel_image
 
         image_spatial =  cv2.resize(image_array_spatial, dsize=self.out_image_size, interpolation=cv2.INTER_AREA)
-        np.save(f'{config.RESIZED_IMAGE_PATH}test.npy', image_spatial)
-        cv2.imwrite(f'{config.RESIZED_IMAGE_PATH}test.png', image_spatial)
-        cv2.imwrite(f'{config.RESIZED_IMAGE_PATH}tesdt.png', image_array)
-        return image_spatial
+        return image_array_spatial
 
     def yield_image_array(self):
         for image_path in self.image_paths:
@@ -51,10 +55,11 @@ class DesignImage():
 
 
 
-designImage = DesignImage(images_set = 'train', out_image_size= (224, 224))
+designImage = DesignImage(images_set = 'train', out_image_size= (224, 224),  chl_pos_in_spatial = [0,1,-1,3,4,5])
 # with Pool(1) as p:
 #     p.map(designImage.concat_channels_to_spatial, designImage.yield_image_array())
 
-for i in designImage.yield_image_array():
-    image_spatial = designImage.concat_channels_to_spatial(i)
+for i, image_array in enumerate(designImage.yield_image_array()):
+    image_spatial = designImage.concat_channels_to_spatial(image_array)
+    np.save(f'{config.RESIZED_IMAGE_PATH}test{i}.npy', image_spatial)
     
