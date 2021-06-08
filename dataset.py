c38ffce7e5b402da4c456cf2c128e37aa0520dac
sajwankit <sajwanankit6@gmail.com>
2021-06-08 10:01:21 +0000

    add ext needles

--- dataset.py
+++ dataset.py
@@ -98,24 +98,27 @@ class ImageTransform():
 #   if sample_type == pos_sample: add_needle to 0,2 or 4 channel
 #    else: add_needle to 1,3 or 5 channel or add_needle to 0,1,2,3,4,5 channels
     
-    def add_needle(self, chls_to_add_needle, needle_img):
+    def add_needle(self, chls_to_add_needle, needle_img, blend_prop = 0.5):
         fimg = np.copy(self.image_array)
+        final_shape = fimg.shape
+        chnl_shape = (final_shape[0]//6, final_shape[1]//1) #will be approx to note.
+        f = chnl_shape[1]
+        t = chnl_shape[0]   
         for chl in chls_to_add_needle:
-            #code to add needle to channel
-            # [chl*t:(chl+1)*t, : f]
-        return fimg
+            fimg[chl*t:(chl+1)*t, : f] = (1 - blend_prop)*fimg[chl*t:(chl+1)*t, : f] + blend_prop*needle_img
+        return self.normalize(fimg).astype(float32)
 
     def apply_ext_needle(self):
         ftarget_type = random.choice([0, 1])
-        needle_type = random.choice(['nb', 'nbd', 'spnb', 'squ', 'sspnb'])
+        needle_type = random.choice(['nb/', 'nbd/', 'spnb/', 'squ/', 'sspnb/'])
         needle_path = f'{config.NEEDLE_PATH}{needle_type}'
-        needle_img = cv2.imread(needle_type, cv2.IMREAD_GRAYSCALE)
-        trans_image_array = np.copy(self.image_array)
+        needle_img = self.normalize(cv2.imread(needle_type, cv2.IMREAD_GRAYSCALE))
 
         if ftarget_type == 1:
             chls_to_add_needle = random.sample([0, 2, 4], random.choice([1, 2, 3]))
             trans_image_array = add_needle(chls_to_add_needle, needle_img)
         else:
+            needle_img = np.amax(needle_img) - needle_img
             chls_to_add_needle = random.sample([1, 3, 5], random.choice([1, 2, 3]))
             trans_image_array = add_needle(chls_to_add_needle, needle_img)
         return trans_image_array
