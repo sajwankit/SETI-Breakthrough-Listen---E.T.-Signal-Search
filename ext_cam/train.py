5157af4a2c26f7fbc8283630026cf9201a091d09
sajwankit <sajwanankit6@gmail.com>
2021-06-11 23:54:32 +0000

    needle cam

--- ext_cam/train.py
+++ ext_cam/train.py
@@ -18,6 +18,13 @@ import seedandlog
 
 from torch.cuda import amp
 torch.multiprocessing.set_sharing_strategy('file_system')
+
+def multi_class_accuacy(predictions, targets):
+    winners = predictions.argmax(dim=1)
+    corrects = (winners == targets)
+    accuracy = corrects.sum().float() / float( targets.size(0) )
+    return accuracy
+
 if __name__ == '__main__':
     seedandlog.seed_torch(seed=config.SEED)
     date_time = config.DATETIME
@@ -34,18 +41,17 @@ if __name__ == '__main__':
 
 
     needle_target_encoding = {
-                'brightpixel':0,
-                'narrowband': 1,
-                'narrowbanddrd': 2,
-                'noise': 3, 
-                'squarepulsednarrowand': 4,
-                'squiggle': 5,
-                'squigglesquarepulsednarrowband': 6
+                'brightpixel':[1, 0, 0, 0, 0, 0, 0],
+                'narrowband': [0, 1, 0, 0, 0, 0, 0],
+                'narrowbanddrd': [0, 0, 1, 0, 0, 0, 0],
+                'noise': [0, 0, 0, 1, 0, 0, 0], 
+                'squarepulsednarrowand': [0, 0, 0, 0, 1, 0, 0],
+                'squiggle': [0, 0, 0, 0, 0, 1, 0],
+                'squigglesquarepulsednarrowband': [0, 0, 0, 0, 0, 0, 1]
                 }
 
-
     train_image_paths = glob.glob(f'{config.NEEDLE_PATH}*train/*.png')
-    train_targets = [needle_target_encoding(x.split('/')[-2]) for p in train_image_paths]
+    train_targets, train_ids = [needle_target_encoding(x.split('/')[-2]), (x.split('/')[-1]).split('_')[0] for x in train_image_paths]
 
     logger = seedandlog.init_logger(log_name = f'{config.MODEL_NAME}_bs{bs}_size{config.IMAGE_SIZE[0]}_mixup{config.MIXUP}_aug{config.AUG}_dt{date_time}')
     logger.info(f'fold,epoch,val_loss,val_auc,tr_auc, train_loss, time')
@@ -55,6 +61,7 @@ if __name__ == '__main__':
 
     train_dataset = dataset.SetiNeedleDataset(image_paths = train_image_paths,
                                                     targets = train_targets,
+                                                    ids = train_ids,
                                                     resize = None,
                                                     augmentations = True)
     
@@ -66,10 +73,11 @@ if __name__ == '__main__':
 
 
     valid_image_paths = glob.glob(f'{config.NEEDLE_PATH}*valid/*.png')
-    valid_targets = [needle_target_encoding(x.split('/')[-2]) for p in valid_image_paths]
+    valid_targets, valid_ids = [needle_target_encoding(x.split('/')[-2]), (x.split('/')[-1]).split('_')[0] for x in train_image_paths]
 
     valid_dataset = dataset.SetiNeedleDataset(image_paths = valid_image_paths,
                                                     targets = valid_targets,
+                                                    ids = valid_ids
                                                     resize = None,
                                                     augmentations = True)
     
@@ -100,12 +108,12 @@ if __name__ == '__main__':
         train_predictions, train_targets, train_ids, train_loss = engine.train(train_loader, model, optimizer, device, scaler)
         predictions, valid_targets, valid_ids, valid_loss = engine.evaluate(valid_loader, model, device)
         scheduler.step(valid_loss)
-        train_roc_auc = metrics.roc_auc_score(train_targets, train_predictions)
-        valid_roc_auc = metrics.roc_auc_score(valid_targets, predictions)
+        train_acc = multi_class_accuacy(train_predictions, train_targets)
+        valid_acc = multi_class_accuacy(predictions, valid_targets)
         et = time.time()
 
         # train auc doesnot make sense when using mixup
-        logger.info(f'{fold},{epoch},{valid_loss},{valid_roc_auc},{train_roc_auc},{train_loss}, {(et-st)/60}')
+        logger.info(f'{fold},{epoch},{valid_loss},{valid_acc},{train_acc},{train_loss}, {(et-st)/60}')
         
         if valid_loss <= best_valid_loss:
             best_valid_loss = valid_loss
@@ -115,8 +123,8 @@ if __name__ == '__main__':
                         'valid_targets': valid_targets},
                         f'{config.MODEL_OUTPUT_PATH}loss_{config.MODEL_NAME}_fold{fold}_bs{bs}_size{config.IMAGE_SIZE[0]}_mixup{config.MIXUP}_aug{config.AUG}_dt{config.DATETIME}.pth')
 
-        if valid_roc_auc >= best_valid_roc_auc:
-            best_valid_roc_auc = valid_roc_auc
+        if valid_acc >= best_valid_acc:
+            best_valid_acc = valid_acc
             torch.save({'model': model.state_dict(),
                         'valid_ids': valid_ids,
                         'predictions': predictions,
