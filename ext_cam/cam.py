026dde53833b39f4c2611e8652e17230c2aac9cd
sajwankit <sajwanankit6@gmail.com>
2021-06-12 21:04:48 +0000

    wipcam

--- ext_cam/cam.py
+++ ext_cam/cam.py
@@ -1,237 +1,136 @@
-import pandas as pd
 import numpy as np
-from timm.models.senet import _weight_init
 import torch
-from sklearn import metrics
-
 import config
 import dataset
-import engine
 import models
 import seedandlog
-import os
 import cv2
-from tqdm import tqdm
 import torch
 import glob
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    seedandlog.seed_torch(seed=config.SEED)
+seedandlog.seed_torch(seed=config.SEED)
 
-    data_path = config.DATA_PATH
-    device = config.DEVICE
-    bs = config.BATCH_SIZE
-    target_size = config.TARGET_SIZE
+def returnCAM(feature_conv, weight, class_idx):
+    # generate the class activation maps upsample
+    size_upsample = (384, 512)
+    bz, nc, h, w = feature_conv.shape
+    output_cam = []
+    for idx in class_idx:
+        cam = weight[idx].dot(feature_conv[0,:, :, ].reshape((nc, h*w)))
+        cam = cam.reshape(h, w)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        output_cam.append(cv2.resize(cam_img, size_upsample))
+    return output_cam
 
-    model = models.Model(pretrained = False)
-    model.to(device)
-    state = torch.load(f'{config.MODEL_OUTPUT_PATH}{config.MODEL_LOAD_FOR_INFER}_{config.MODEL_NAME}_bs{bs}_size{config.IMAGE_SIZE[0]}_dt{config.DATETIME}.pth')
+class SaveFeatures():
+    def __init__(self, m):
+        """ Extract pretrained activations"""
+        self.hook = m.register_forward_hook(self.hook_fn)
+        self.features = None
+    def hook_fn(self, module, input, output):
+        self.features = output.data.cpu().numpy()
+    def remove(self):
+        self.hook.remove()
 
+data_path = config.DATA_PATH
+device = config.DEVICE
+bs = config.BATCH_SIZE
+target_size = config.TARGET_SIZE
 
-    needle_target_encoding = {
-                'brightpixel':[1, 0, 0, 0, 0, 0, 0],
-                'narrowband': [0, 1, 0, 0, 0, 0, 0],
-                'narrowbanddrd': [0, 0, 1, 0, 0, 0, 0],
-                'noise': [0, 0, 0, 1, 0, 0, 0], 
-                'squarepulsednarrowband': [0, 0, 0, 0, 1, 0, 0],
-                'squiggle': [0, 0, 0, 0, 0, 1, 0],
-                'squigglesquarepulsednarrowband': [0, 0, 0, 0, 0, 0, 1]
-                }
-
-    test_image_paths = glob.glob(f'{config.DATA_PATH}valid/*/*.png')
-    test_targets = [needle_target_encoding[x.split('/')[-2]] for x in test_image_paths]
-    test_ids = [int((x.split('/')[-1]).split('_')[0]) for x in test_image_paths]
-
-    test_dataset = dataset.SetiNeedleDataset(image_paths = test_image_paths, ids = test_ids, targets = test_targets)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=config.BATCH_SIZE,
-                                            shuffle=False, 
-                                        num_workers=4)
-
-    model.load_state_dict(state['model'])                                    
-    
-    for param in model.parameters():
-        param.requires_grad = False
-
-    print(model)
-    model.eval()
-    # finalconv_name = 'layer4'
-    # # hook the feature extractor
-    # features_blobs = []
-    # def hook_feature(module, input, output):
-    #     features_blobs.append(output.data.cpu().numpy())
-
-    conv_layer = model.model.layer4[1]._modules.get('conv2')
-    print(conv_layer)
-    # get the softmax weight
-    params = list(model.parameters())
-    fc_params = list(model.model._modules.get('last_linear').parameters())
-    weight = np.squeeze(fc_params[0].cpu().data.numpy())
-    print(fc_params, '\n', weight)
-
-    def returnCAM(feature_conv, weight, class_idx):
-        # generate the class activation maps upsample to 256x256
-        size_upsample = (384, 512)
-        bz, nc, h, w = feature_conv.shape
-        output_cam = []
-        for idx in class_idx:
-            cam = weight[idx].dot(feature_conv[0,:, :, ].reshape((nc, h*w)))
-            cam = cam.reshape(h, w)
-            cam = cam - np.min(cam)
-            cam_img = cam / np.max(cam)
-            output_cam.append(cv2.resize(cam_img, size_upsample))
-        return output_cam
-
-    # predictions = engine.predict(test_loader, model, device)
+model = models.Model(pretrained = False)
+model.to(device)
+state = torch.load(f'{config.MODEL_OUTPUT_PATH}{config.MODEL_LOAD_FOR_INFER}_{config.MODEL_NAME}_bs{bs}_size{config.IMAGE_SIZE[0]}_dt{config.DATETIME}.pth')
 
 
-    # generate class activation mapping for the top1 prediction
-    # CAMs = returnCAM(features_blobs[0], weight, [0])
+# needle_target_encoding = {
+#             'brightpixel':[1, 0, 0, 0, 0, 0, 0],
+#             'narrowband': [0, 1, 0, 0, 0, 0, 0],
+#             'narrowbanddrd': [0, 0, 1, 0, 0, 0, 0],
+#             'noise': [0, 0, 0, 1, 0, 0, 0], 
+#             'squarepulsednarrowband': [0, 0, 0, 0, 1, 0, 0],
+#             'squiggle': [0, 0, 0, 0, 0, 1, 0],
+#             'squigglesquarepulsednarrowband': [0, 0, 0, 0, 0, 0, 1]
+#             }
 
-    class SaveFeatures():
-        def __init__(self, m):
-            """ Extract pretrained activations"""
-            self.hook = m.register_forward_hook(self.hook_fn)
-            self.features = None
-        def hook_fn(self, module, input, output):
-            self.features = output.data.cpu().numpy()
-        def remove(self):
-            self.hook.remove()
+# test_image_paths = glob.glob(f'{config.DATA_PATH}valid/*/*.png')
+# test_targets = [needle_target_encoding[x.split('/')[-2]] for x in test_image_paths]
+# test_ids = [int((x.split('/')[-1]).split('_')[0]) for x in test_image_paths]
 
-        # # hook the feature extractor
-    # features_blobs = []
-    # def hook_feature(module, input, output):
-    #     features_blobs.append(output.data.cpu().numpy())
+# test_dataset = dataset.SetiNeedleDataset(image_paths = test_image_paths, ids = test_ids, targets = test_targets)
+# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=config.BATCH_SIZE,
+#                                         shuffle=False, 
+#                                     num_workers=4)
 
-    # net._modules.get(finalconv_name).register_forward_hook(hook_feature)
+model.load_state_dict(state['model'])                                    
 
-    for i, data in enumerate(test_loader):
-        images = data['images']
-        targets = data['targets']
-        ids = data['ids']
+for param in model.parameters():
+    param.requires_grad = False
 
-        images = images.to(device, dtype = torch.float)
-        activated_features = SaveFeatures(conv_layer)
+model.eval()
 
-        outputs = model(images)
-        outputs = torch.sigmoid(outputs).detach().cpu().numpy().tolist()
+conv_layer = model.model.layer4[1]._modules.get('conv2')
+print(conv_layer)
 
-        # features_blobs = []
-        # def hook_feature(module, input, output):
-        #     features_blobs.append(output.data.cpu().numpy())
+fc_params = list(model.model._modules.get('last_linear').parameters())
+weight = np.squeeze(fc_params[0].cpu().data.numpy())
 
-        
-            # # hook the feature extractor
-        # features_blobs = []
-        # def hook_feature(module, input, output):
-        #     features_blobs.append(output.data.cpu().numpy())
-        # feature_conv = model.model.layer4[1]._modules.get('conv2')
-        # print(feature_conv)
-        # model.model.layer4[1]._modules.get('conv2').register_forward_hook(hook_feature)
+# for i, data in enumerate(test_loader):
+#     images = data['images']
+#     targets = data['targets']
+#     ids = data['ids']
 
-        
-        class_predictions = np.argmax(np.array(outputs), axis = 1)
-        heatmaps = returnCAM(activated_features.features, weight, class_predictions)
+#     images = images.to(device, dtype = torch.float)
+#     activated_features = SaveFeatures(conv_layer)
 
+#     outputs = model(images)
+#     outputs = torch.sigmoid(outputs).detach().cpu().numpy().tolist()
 
+#     class_predictions = np.argmax(np.array(outputs), axis = 1)
+#     heatmaps = returnCAM(activated_features.features, weight, class_predictions)
 
+#     heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+#     result = heatmap * 0.3 + img * 0.5
+#     cv2.imwrite('CAM.jpg', result)
 
+def generate_heatmaps(images):
+    # input: batch of image arrays of shape : (bs, 3, image_size[0], image_size[1])
+    images = images.to(device, dtype = torch.float)
+    activated_features = SaveFeatures(conv_layer)
 
+    outputs = model(images)
+    outputs = torch.sigmoid(outputs).detach().cpu().numpy().tolist()
 
-#     # simple implementation of CAM in PyTorch for the networks such as ResNet, DenseNet, SqueezeNet, Inception
+    class_predictions = np.argmax(np.array(outputs), axis = 1)
+    heatmaps = returnCAM(activated_features.features, weight, class_predictions)
+    return heatmaps, class_predictions
 
-# import io
-# import requests
-# from PIL import Image
-# from torchvision import models, transforms
-# from torch.autograd import Variable
-# from torch.nn import functional as F
-# import numpy as np
-# import cv2
-# import pdb
+def plot_heatmaps(heatmaps, images, class_predictions=None):
+    print(class_predictions)
+    for i, hm in enumerate(heatmaps):
+        hm = cv2.applyColorMap(hm, cv2.COLORMAP_JET)
+        result = hm * 0.3 + images[i] * 0.5
+        plt.imshow(result)
+        plt.show()
+    print('\n\n')
 
-# # input image
-# LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-# IMG_URL = 'http://media.mlive.com/news_impact/photo/9933031-large.jpg'
+test_images_paths = [
+    '/content/primary_small/train/squigglesquarepulsednarrowband/3_squigglesquarepulsednarrowband.png',
+    '/content/primary_small/train/squiggle/88_squiggle.png',
+    '/content/primary_small/train/squarepulsednarrowband/125_squarepulsednarrowband.png',
+    '/content/primary_small/train/narrowbanddrd/118_narrowbanddrd.png',
+    '/content/primary_small/train/narrowband/92_narrowband.png'
+]
+test_images = []
+for p in test_images_paths:
+    imt = dataset.ImageTransform()
+    image = imt.normalize(cv2.imread(p, cv2.IMREAD_GRAYSCALE))
+    image = image.reshape(1,image.shape[0],image.shape[1])
+    image = np.repeat(image, 3, axis = 0)
+    test_images.append(test_images)
+test_images = np.array(test_images)
 
-# # networks such as googlenet, resnet, densenet already use global average pooling at the end, so CAM could be used directly.
-# model_id = 1
-# if model_id == 1:
-#     net = models.squeezenet1_1(pretrained=True)
-#     finalconv_name = 'features' # this is the last conv layer of the network
-# elif model_id == 2:
-#     net = models.resnet18(pretrained=True)
-#     finalconv_name = 'layer4'
-# elif model_id == 3:
-#     net = models.densenet161(pretrained=True)
-#     finalconv_name = 'features'
-
-# net.eval()
-
-# # hook the feature extractor
-# features_blobs = []
-# def hook_feature(module, input, output):
-#     features_blobs.append(output.data.cpu().numpy())
-
-# net._modules.get(finalconv_name).register_forward_hook(hook_feature)
-
-# # get the softmax weight
-# params = list(net.parameters())
-# weight_softmax = np.squeeze(params[-2].data.numpy())
-
-# def returnCAM(feature_conv, weight_softmax, class_idx):
-#     # generate the class activation maps upsample to 256x256
-#     size_upsample = (256, 256)
-#     bz, nc, h, w = feature_conv.shape
-#     output_cam = []
-#     for idx in class_idx:
-#         cam = weight_softmax[idx].dot(feature_conv.reshape((nc, h*w)))
-#         cam = cam.reshape(h, w)
-#         cam = cam - np.min(cam)
-#         cam_img = cam / np.max(cam)
-#         cam_img = np.uint8(255 * cam_img)
-#         output_cam.append(cv2.resize(cam_img, size_upsample))
-#     return output_cam
-
-
-# normalize = transforms.Normalize(
-#    mean=[0.485, 0.456, 0.406],
-#    std=[0.229, 0.224, 0.225]
-# )
-# preprocess = transforms.Compose([
-#    transforms.Resize((224,224)),
-#    transforms.ToTensor(),
-#    normalize
-# ])
-
-# response = requests.get(IMG_URL)
-# img_pil = Image.open(io.BytesIO(response.content))
-# img_pil.save('test.jpg')
-
-# img_tensor = preprocess(img_pil)
-# img_variable = Variable(img_tensor.unsqueeze(0))
-# logit = net(img_variable)
-
-# # download the imagenet category list
-# classes = {int(key):value for (key, value)
-#           in requests.get(LABELS_URL).json().items()}
-
-# h_x = F.softmax(logit, dim=1).data.squeeze()
-# probs, idx = h_x.sort(0, True)
-# probs = probs.numpy()
-# idx = idx.numpy()
-
-# # output the prediction
-# for i in range(0, 5):
-#     print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
-
-# # generate class activation mapping for the top1 prediction
-# CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
-
-# # render the CAM and output
-# print('output CAM.jpg for the top1 prediction: %s'%classes[idx[0]])
-# img = cv2.imread('test.jpg')
-# height, width, _ = img.shapesoftmax
-# heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
-# result = heatmap * 0.3 + img * 0.5
-# cv2.imwrite('CAM.jpg', result)
+heatmaps = generate_heatmaps(test_images)
+plot_heatmaps(heatmaps, test_images)
 
