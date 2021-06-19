ab7819a2d9c871b99ea47feeb7a29be6adcd8fbd
sajwankit <sajwanankit6@gmail.com>
2021-06-19 20:51:38 +0000

    arcloss wip

--- src/loss_scheduler.py
+++ src/loss_scheduler.py
@@ -4,43 +4,59 @@ import math
 
 class Loss(nn.modules.Module):
     '''
-    outputs means the output of the last layer of model
+    logits is output of the last layer of model
     '''
-    def __init__(self, outputs, targets):
+    def __init__(self, logits, targets):
         super().__init__()
-        self.outputs = outputs
+        self.logits = logits
         self.targets = targets
     
      
 class BCEWithLogitsLoss(Loss):
     def forward(self):
-        return nn.BCEWithLogitsLoss()(self.outputs, self.targets.view(-1,1))
+        return nn.BCEWithLogitsLoss()(self.logits, self.targets.view(-1,1))
 
 class ArcLoss(Loss):
     '''
     W = Weight at last layer
     x = last layer feature
     Z = logits
-    outputs = logits = Z = W*x = |W||x|cos(theta)
+    logits = logits = Z = W*x = |W||x|cos(theta)
     normalised weights W and normalised features x sent here, |W|=1 |x|=1
-    thus outputs become cos(theta)
+    thus logits become cos(theta)
 
-    MAKE SURE model output takes care of above before using this loss
+    MAKE SURE model logits takes care of above before using this loss
     '''
-    def __init__(self, outputs, targets, feature_scale = 30.0, margin = 0.5, reduction = 'mean'):
-        super().__init__(outputs, targets)
+    def __init__(self, logits, targets, feature_scale=30.0, margin=0.5, reduction='mean'):
+        super().__init__(logits, targets)
         self.feature_scale = feature_scale
         self.margin = margin
         self.reduction = reduction
         self.margin_cos = math.cos(margin)
         self.margin_sin = math.sin(margin)
+        self.th = math.cos(math.pi - margin)
+        self.mm = math.sin(math.pi - margin) * margin
 
     def forward(self):
         '''
-        outputs = logits = cos(theta)
+        logits = logits = cos(theta)
         margin added to theta cos(theta+margin)
         '''
-        outputs = outputs.float()
+        logits = logits.float()
+        logits_to_sine = torch.sqrt(1 - torch.pow(logits, 2))
+        logits_plus_margin = logits*self.margin_cos-logits_to_sine*self.margin_sin
+        logits_plus_margin = torch.where(logits > self.th, logits_plus_margin, logits-self.mm)
+        
+        # labels2 = torch.zeros_like(logits)
+        # labels.scatter_(1, labels.view(-1, 1).long(), 1)
+        targets = self.targets.view(-1,1)
+        logits_plus_margin = (targets*logits_plus_margin)+((1-targets)*logits)
+
+        logits_plus_margin *= self.feature_scale
+
+        loss = nn.BCEWithLogitsLoss()(logits_plus_margin, self.targets.view(-1,1), reduction = reduction)
+        
+        return loss
 
 
 
