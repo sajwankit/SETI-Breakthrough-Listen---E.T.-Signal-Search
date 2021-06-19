63a43666659d8936910336d1ee13618bb30785d5
sajwankit <sajwanankit6@gmail.com>
2021-06-19 17:12:28 +0000

    wip arcloss

---
+++ src/loss_scheduler.py
@@ -0,0 +1,26 @@
+import config
+import torch.nn as nn
+import math
+
+class Loss(nn.modules.Module):
+    def __init__(self, outputs, targets):
+        super().__init__()
+        self.outputs = outputs
+        self.targets = targets
+    
+     
+class BCEWithLogitsLoss(Loss):
+    def forward(self):
+        return nn.BCEWithLogitsLoss()(self.outputs, self.targets.view(-1,1))
+
+class ArcLoss(Loss):
+    def __init__(self, outputs, targets, feature_scale = 30.0, margin = 0.5, reduction = 'mean'):
+        super().__init__(outputs, targets)
+        self.feature_scale = feature_scale
+        self.margin = margin
+        self.reduction = reduction
+        self.margin_cos = math.cos(margin)
+        self.margin_sin = math.sin(margin)
+        
+    def forward
+
