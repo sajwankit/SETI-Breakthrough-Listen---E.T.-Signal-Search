8277332fa4ae2e19b08be1e064bc6577c050f0bb
sajwankit <sajwanankit6@gmail.com>
2021-06-27 05:33:11 +0000

    vae wip

---
+++ vae/train.py
@@ -0,0 +1,271 @@
+import pandas as pd
+import numpy as np
+import glob
+import argparse
+import time
+
+import albumentations
+import torch
+
+from sklearn import metrics
+
+import utils
+import config
+import dataset
+import engine
+import models
+import validation_strategy as vs
+import seedandlog
+import sampler
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from torch.cuda import amp
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+if __name__ == '__main__':
+    seedandlog.seed_torch(seed=config.SEED)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fold', type = int)
+    args = parser.parse_args()
+
+    saved_model_name = config.SAVED_MODEL_NAME
+
+    data_path = config.DATA_PATH
+    device = config.DEVICE
+    epochs = config.EPOCHS
+    bs = config.BATCH_SIZE
+    lr = config.INIT_LEARNING_RATE
+    target_size = config.TARGET_SIZE
+
+    df = pd.read_csv(data_path+'train_labels.csv')
+    if config.DEBUG:
+        ids = [x.split('/')[-1].split('.')[0] for x in glob.glob(f'{config.RESIZED_IMAGE_PATH}train/*.npy')][:320]
+        df = df[df['id'].isin(ids)]
+        df.reset_index(inplace = True, drop = True)
+    images = list(glob.glob(data_path+'train/*'))
+    targets = df.target.values
+
+    '''
+    stratify based on target and image group
+    ''' 
+    skFoldData = vs.get_SKFold(ids = df.index.values,
+                               targets = targets,
+                               n_folds = config.FOLDS,
+                               seed = config.SEED,
+                               shuffle = True)
+
+
+    '''
+    stratify based on target and image group
+    ''' 
+#     id_keys_map = {'a':10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15}
+#     id_keys = []
+#     for key in df['id'].values.tolist():
+#         try:
+#             key = int(key[0])
+#         except:
+#             key = id_keys_map[key[0]]
+#         id_keys.append(key)
+#     target = df['target'].values.tolist()
+#     multi_targets = [ [id_keys[x], target[x]] for x in range(len(df))]
+
+#     mskFoldData = vs.get_MSKFold(ids = df.index.values,
+#                                 multi_targets = np.array(multi_targets),
+#                                 nfolds = config.FOLDS,
+#                                 seed = config.SEED)
+
+    logger = seedandlog.init_logger(log_name = f'{saved_model_name}')
+    logger.info(f'fold,epoch,val_loss,val_rec, val_kld, train_loss, train_rec, train_kld, time')
+
+    for fold, foldData in enumerate(skFoldData):
+        if fold == args.fold or args.fold is None:
+        
+            #for every fold model should start from zero training
+            if config.NET == 'VAE':
+                model = vae.VariationalAutoencoder(pretrained=True, )
+            else:
+                model = models.get_model(pretrained=True, net_out_features=config.TARGET_SIZE)
+            model.to(device)
+            
+            if config.LOAD_SAVED_MODEL:
+                states = torch.load(f'{config.MODEL_OUTPUT_PATH}auc_fold{fold}_{saved_model_name}.pth')
+                model.load_state_dict(states['model'])
+                optimizer.load_state_dict(states['optimizer'])
+                scheduler.load_state_dict(states['scheduler'])
+                scaler.load_state_dict(states['scaler'])
+                print('Saved Model LOADED!')
+                start_epoch = states['epoch']
+            else:
+                start_epoch = 0
+                
+            trIDs = foldData['trIDs']
+            vIDs = foldData['vIDs']
+    
+            train_images_path = []
+            train_targets = []
+            for id in trIDs:
+                if config.ORIG_IMAGE:
+#                     for original images
+                    train_images_path.append(data_path+'train/'+str(df.loc[int(id),'id'])[0]+'/'+str(df.loc[int(id),'id'])+'.npy')
+                    train_targets.append(int(df.loc[int(id), 'target']))
+                else:
+#                    for resized images
+                    filename = df.loc[int(id),'id']
+                    train_images_path.append(f'{config.RESIZED_IMAGE_PATH}train/{filename}.npy')
+                    train_targets.append(int(df.loc[int(id), 'target']))
+
+            if config.DEBUG:
+                train_targets = (np.random.rand(240) > 0.9).astype(int)
+
+            train_dataset = dataset.SetiDataset(image_paths = train_images_path,
+                                                    targets = train_targets,
+                                                    ids = trIDs,
+                                                    resize = None,
+                                                    augmentations = True)
+            
+#             train_loader = torch.utils.data.DataLoader(train_dataset, pin_memory = True,
+#                                                         batch_sampler = sampler.StratifiedSampler( ids = trIDs,
+#                                                                                             targets = train_targets,
+#                                                                                             batch_size = config.BATCH_SIZE),                              
+#                                                 num_workers = 8,
+#                                                 worker_init_fn = seedandlog.seed_torch(seed=config.SEED)
+#                                                 )
+
+
+            train_loader = torch.utils.data.DataLoader(train_dataset,
+                                                batch_size = bs,
+                                                shuffle = True,
+                                                num_workers = 4,
+                                                worker_init_fn = seedandlog.seed_torch(seed=config.SEED),
+                                                      pin_memory = True)
+    
+            valid_images_path = []
+            valid_targets = []
+            for id in vIDs:
+                if config.ORIG_IMAGE:
+#                 for original images
+                    valid_images_path.append(data_path+'train/'+str(df.loc[int(id),'id'])[0]+'/'+str(df.loc[int(id),'id'])+'.npy')
+                    valid_targets.append(int(df.loc[int(id), 'target']))
+                else:
+#                     for resized images
+                    filename = df.loc[int(id),'id']
+                    valid_images_path.append(f'{config.RESIZED_IMAGE_PATH}train/{filename}.npy')
+                    valid_targets.append(int(df.loc[int(id), 'target']))
+
+            if config.DEBUG:
+                valid_targets = (np.random.rand(80) > 0.9).astype(int)
+
+            valid_dataset = dataset.SetiDataset(image_paths = valid_images_path,
+                                                targets = valid_targets,
+                                                ids = vIDs,
+                                                resize = None,
+                                                augmentations = False)
+                                                    
+            valid_loader = torch.utils.data.DataLoader(valid_dataset,
+                                                        batch_size = bs,
+                                                        shuffle = True,
+                                                        num_workers = 4,
+                                                        worker_init_fn = seedandlog.seed_torch(seed=config.SEED),
+                                                      pin_memory = True)
+
+#             valid_loader = torch.utils.data.DataLoader(valid_dataset, pin_memory = True,
+#                                                         batch_sampler = sampler.StratifiedSampler( ids = vIDs,
+#                                                                                             targets = valid_targets,
+#                                                                                             batch_size = config.BATCH_SIZE),                              
+#                                                 num_workers = 8,
+#                                                 worker_init_fn = seedandlog.seed_torch(seed=config.SEED)
+#                                                 )
+
+            optimizer, scheduler = utils.OptSch(opt=config.OPTIMIZER, sch=config.SCHEDULER).get_opt_sch(model)
+
+            if config.MIXED_PRECISION:
+                #mixed precision training
+                scaler = amp.GradScaler()
+            else:
+                scaler = None
+
+            best_valid_loss = 999
+            best_valid_roc_auc = -999
+
+            '''
+            Training ON
+            '''
+            for epoch in range(start_epoch, epochs):
+                
+                if config.OHEM_LOSS:
+                    if epoch >= (config.EPOCHS -12):
+                        initial_rate = 1
+                        final_rate = 0.4
+                        config.OHEM_RATE = initial_rate + (epoch - (config.EPOCHS-12))*(final_rate - initial_rate)/( (config.EPOCHS-1) - (config.EPOCHS-12) )
+                        print(f'applying ohem with rate {config.OHEM_RATE}')
+                    else:
+                        config.OHEM_RATE = 1
+                
+                st = time.time()
+                if config.NET == 'NetArcFace':
+                    train_prediction_confs, train_predictions, train_targets, train_ids, train_loss = engine.train(train_loader, model, optimizer, device, scaler)
+                    prediction_confs, predictions, valid_targets, valid_ids, valid_loss = engine.evaluate(valid_loader, model, device)
+#                     train_roc_auc = metrics.roc_auc_score(np.array(train_targets), np.array(train_predictions)[:,1])
+#                     valid_roc_auc = metrics.roc_auc_score(np.array(valid_targets), np.array(predictions)[:,1])
+                else:
+                    if config.NET == 'VAE':
+                        train_predictions, train_targets, train_ids, train_loss = engine.train(train_loader, model, optimizer, device, scaler)
+                        predictions, valid_targets, valid_ids, valid_loss = engine.evaluate(valid_loader, model, device)
+                    else:
+                        train_predictions, train_targets, train_ids, train_loss = engine.train(train_loader, model, optimizer, device, scaler)
+                        predictions, valid_targets, valid_ids, valid_loss = engine.evaluate(valid_loader, model, device)
+                train_roc_auc = metrics.roc_auc_score(np.array(train_targets), np.array(train_predictions))
+                valid_roc_auc = metrics.roc_auc_score(np.array(valid_targets), np.array(predictions))
+                    
+                if config.SCHEDULER == 'ReduceLROnPlateau':
+                    scheduler.step(valid_loss)
+                else:
+                    scheduler.step()
+
+                et = time.time()
+
+                # train auc doesnot make sense when using mixup
+                logger.info(f'{fold},{epoch},{valid_loss},{valid_roc_auc},{train_roc_auc},{train_loss}, {(et-st)/60}')
+                
+                if valid_loss <= best_valid_loss:
+                    best_valid_loss = valid_loss
+                    torch.save({'model': model.state_dict(),
+                                'optimizer': optimizer.state_dict(),
+                                'scheduler': scheduler.state_dict(),
+                                'scaler': scaler.state_dict(),
+                                'epoch': epoch,
+                                'valid_ids': valid_ids,
+                                'predictions': predictions,
+                                'prediction_confs': prediction_confs if config.NET == 'NetArcFace' else 1,
+                                'valid_targets': valid_targets},
+                                f'{config.MODEL_OUTPUT_PATH}loss_fold{fold}_{saved_model_name}.pth')
+
+                if valid_roc_auc >= best_valid_roc_auc:
+                    best_valid_roc_auc = valid_roc_auc
+                    torch.save({'model': model.state_dict(),
+                                'optimizer': optimizer.state_dict(),
+                                'scheduler': scheduler.state_dict(),
+                                'scaler': scaler.state_dict(),
+                                'epoch': epoch,
+                                'valid_ids': valid_ids,
+                                'predictions': predictions,
+                                'prediction_confs': prediction_confs if config.NET == 'NetArcFace' else 1,
+                                'valid_targets': valid_targets},
+                                f'{config.MODEL_OUTPUT_PATH}auc_fold{fold}_{saved_model_name}.pth')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vae/vae.py
+++ vae/vae.py
@@ -3,45 +3,164 @@ from abc import abstractmethod
 import timm
 import models
 
-class BaseVAE(nn.Module):
+# class BaseVAE(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+    
+#     def encode(self, x):
+#         raise NotImplementedError
+    
+#     def decode(self, z):
+#         raise NotImplementedError
+    
+#     def sample(self, batch_size, current_device, **kwargs):
+#         raise RuntimeWarning()
+    
+#     def generate(self, x, **kwargs):
+#         raise NotImplementedError
+    
+#     def forward(self, x):
+#         pass
+    
+#     def loss_function(self, recon_x, x, mu, logvar):
+#         pass
+
+# class CNNVAE(BaseVAE):
+#     def __init__(self, kldw=0.5, pretrained=True, in_channels=3, latent_dim=2, hidden_dims=None, **kwargs):
+#         super(BaseVAE).__init__()
+#         self.laten_dim = latent_dim
+#         self.kldw =  self.kldw
+#         '''
+#         Build Encoder
+#         '''
+#         self.encoder_backbone = models.Backbone(pretrained=pretrained)
+#         self.fc_mu = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dim)
+#         self.fc_var = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dim)
+
+#         '''
+#         Build Decoder
+#         '''
+#         modules = []
+#         if hidden_dims is None:
+#             hidden_dims = [32, 64, 128, 256, 512]
+
+#         self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1]*4)
+#         hidden_dims.reverse()
+
+#         for i in range(len(hidden_dims)-1):
+#             modules.append(
+#                 nn.Sequential(
+#                     nn.ConvTranspose2d(hidden_dims[i],
+#                                        hidden_dims[i + 1],
+#                                        kernel_size=3,
+#                                        stride = 2,
+#                                        padding=1,
+#                                        output_padding=1),
+#                     nn.BatchNorm2d(hidden_dims[i + 1]),
+#                     nn.LeakyReLU())
+#             )
+        
+#         self.decoder = nn.Sequential(*modules)
+
+#         self.final_layer = nn.Sequential(
+#                             nn.ConvTranspose2d(hidden_dims[-1],
+#                                                hidden_dims[-1],
+#                                                kernel_size=3,
+#                                                stride=2,
+#                                                padding=1,
+#                                                output_padding=1),
+#                             nn.BatchNorm2d(hidden_dims[-1]),
+#                             nn.LeakyReLU(),
+#                             nn.Conv2d(hidden_dims[-1], out_channels= in_channels,
+#                                       kernel_size= 3, padding= 1),
+#                             nn.Tanh())
+        
+#     def encoder(self, x):
+#         x = self.encoder_backbone(x)
+#         mu = self.fc_mu(x)
+#         logvar = self.fc_var(x)
+
+#         return [mu, logvar]
+    
+#     def decoder(self, z):
+#         '''
+#         Maps the given latent vector onto the image space
+#         '''
+#         z = self.decoder_input(z)
+#         z = self.decoder(z)
+#         z = self.final_layer(z)
+#         return z
+    
+#     def reparameterize(self, mu, logvar):
+#         '''
+#         Reparameterization trick to sample from N(mu, var) from
+#         N(0,1).
+#         :param mu: (Tensor) Mean of the latent Gaussian [B x D]
+#         :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
+#         :return: (Tensor) [B x D]
+#         '''
+#         std = logvar.mul(0.5).exp_()
+#         eps = torch.empty_like(std).normal_()
+#         return eps*std+mu
+    
+#     def forward(self, x):
+#         mu, logvar = self.encoder(x)
+#         if self.training:
+#             z = self.reparameterize(mu, logvar)
+#         else:
+#             return mu
+    
+#     def loss_function(self, recon_x, x, mu, logvar):
+#         recon_loss = nn.functional.mse_loss(recon_x, x)
+#         kld_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()), dim=1, dim=0)
+
+#         loss = recon_loss + kld_weight * kld_loss
+#         return {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD':-kld_loss}
+
+#      def sample(self, z
+#                num_samples,
+#                device):
+#         """
+#         Samples from the latent space and return the corresponding
+#         image space map.
+#         :param num_samples: (Int) Number of samples
+#         :param device: (Int) Device to run the model
+#         :return: (Tensor)
+#         """
+#         z = torch.randn(num_samples,
+#                         self.latent_dim)
+
+#         z = z.to(device)
+
+#         samples = self.decode(z)
+#         return samples
+
+#     def generate(self, x) -> Tensor:
+#         """
+#         Given an input image x, returns the reconstructed image
+#         :param x: (Tensor) [B x C x H x W]
+#         :return: (Tensor) [B x C x H x W]
+#         """
+
+#         return self.forward(x)[0]
+
+
+class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
-    
-    def encode(self, input):
-        raise NotImplementedError
-    
-    def decode(self, input):
-        raise NotImplementedError
-    
-    def sample(self, batch_size, current_device, **kwargs):
-        raise RuntimeWarning()
-    
-    def generate(self, x, **kwargs):
-        raise NotImplementedError
-    
-    @abstractmethod
-    def forward(self, inputs):
-        pass
-    
-    @abstractmethod
-    def loss_function(self, inputs):
-        pass
-
-class CNNVAE(BaseVAE):
-    def __init__(self, pretrained=True, in_channels=3, latent_dim=2, hidden_dims=None, **kwargs):
-        super(BaseVAE).__init__()
-        self.laten_dim = latent_dim
-        
-        '''
-        Build Encoder
-        '''
         self.encoder_backbone = models.Backbone(pretrained=pretrained)
-        self.fc_mu = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dim)
-        self.fc_var = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dim)
+        self.fc_mu = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dims)
+        self.fc_logvar = nn.Linear(self.encoder_backbone.model.last_linear.out_features, latent_dims)
 
-        '''
-        Build Decoder
-        '''
+    def forward(self, x):
+        x = self.encoder_backbone(x)
+        x_mu = self.fc_mu(x)
+        x_logvar = self.fc_logvar(x)
+        return x_mu, x_logvar
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
         modules = []
         if hidden_dims is None:
             hidden_dims = [32, 64, 128, 256, 512]
@@ -62,7 +181,7 @@ class CNNVAE(BaseVAE):
                     nn.LeakyReLU())
             )
         
-        self.decoder = nn.Sequential(*modules)
+        self.decoder_backbone = nn.Sequential(*modules)
 
         self.final_layer = nn.Sequential(
                             nn.ConvTranspose2d(hidden_dims[-1],
@@ -76,23 +195,19 @@ class CNNVAE(BaseVAE):
                             nn.Conv2d(hidden_dims[-1], out_channels= in_channels,
                                       kernel_size= 3, padding= 1),
                             nn.Tanh())
-        
-    def encode(self, x):
-        x = self.encoder_backbone(x)
-        mu = self.fc_mu(x)
-        logvar = self.fc_var(x)
-
-        return [mu, logvar]
-    
-    def decode(self, z):
-        '''
-        Maps the given latent vector onto the image space
-        '''
+    def forward(self, z):
         z = self.decoder_input(z)
-        z = self.decoder(z)
+        z = self.decoder_backbone(z)
         z = self.final_layer(z)
         return z
-    
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.kldw =  self.kldw
+
     def reparameterize(self, mu, logvar):
         '''
         Reparameterization trick to sample from N(mu, var) from
@@ -101,16 +216,24 @@ class CNNVAE(BaseVAE):
         :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
         :return: (Tensor) [B x D]
         '''
-        std = logvar.mul(0.5).exp_()
-        eps = torch.empty_like(std).normal_()
-        return eps*std+mu
-    
-    def forward(self, x):
-        mu, logvar = self.encode(x)
         if self.training:
-            z = self.reparameterize(mu, logvar)
+            # the reparameterization trick
+            std = logvar.mul(0.5).exp_()
+            eps = torch.empty_like(std).normal_()
+            return eps.mul(std).add_(mu)
         else:
             return mu
+
+    def forward(self, x):
+        latent_mu, latent_logvar = self.encoder(x)
+        latent = self.reparameterize(latent_mu, latent_logvar)
+        x_recon = self.decoder(latent)
+        return x_recon, latent_mu, latent_logvar
+
+    def loss_function(self, recon_x, x, mu, logvar):
+        recon_loss = nn.functional.mse_loss(recon_x, x, reduction='mean')
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()), dim=1, dim=0)
+
+        loss = recon_loss + self.kldw * kld_loss
+        return {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD':-kld_loss}
     
-    def loss_function(self):
-        
