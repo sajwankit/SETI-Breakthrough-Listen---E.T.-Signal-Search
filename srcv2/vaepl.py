fee8aac6dd1e6810a06cae65981a8505f57171ac
sajwankit <sajwanankit6@gmail.com>
2021-07-25 10:50:26 +0000

    vae try2

--- srcv2/vaepl.py
+++ srcv2/vaepl.py
@@ -11,13 +11,14 @@ from pl_bolts.models.autoencoders.components import (
     resnet18_decoder,
     resnet18_encoder,
 )
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 import vae
 import models
 import config
 import dataset
 import seedandlog
 import os
+import glob
 pre_classifier = models.get_model(pretrained=True, net_out_features=config.TARGET_SIZE)
 class SaveFeatures():
     def __init__(self, m):
@@ -124,3 +125,41 @@ class VAEPL(pl.LightningModule):
         })
 
         return elbo
+
+imgset = 'train'
+def get_test_file_path(image_id):
+    if config.IMAGE_TYPE == 'orig':
+        return f"{data_path}{imgset}/{image_id[0]}/{image_id}.npy"
+    elif config.IMAGE_TYPE == 'norm':
+        return f"{config.NORM_IMAGE_PATH}{imgset}/{image_id}.npy__"
+    elif config.IMAGE_TYPE == 'resized':
+        return f"{config.RESIZED_IMAGE_PATH}{imgset}/{image_id}.npy"
+
+if config.DEBUG:
+    df = pd.read_csv(data_path+'train_labels.csv')[:10]
+else:
+    df = pd.read_csv(data_path+'train_labels.csv')
+
+# glob.glob(f"{data_path}{imgset}/{image_id[0]}/{image_id}.npy")
+df['image_path'] = df['id'].apply(get_test_file_path)
+df['orig_index'] = df.index.values
+
+inputs = dataset.SetiDataset(df=df, pred=False, augmentations=False)
+
+inputs_loader = torch.utils.data.DataLoader(inputs,
+                                                batch_size = 32,
+                                                shuffle = True,
+                                                num_workers = 4,
+                                                worker_init_fn = seedandlog.seed_torch(seed=config.SEED),
+                                                      pin_memory = True)
+
+
+ckp_clbk = ModelCheckpoint(dirpath='/home/asajw/SETI/notebooks/',
+                       monitor='elbo',
+                        filename='m-0ch-recon-vae_{epoch:02d}_{elbo:.2f}'
+                       )
+pl.seed_everything(1234)
+
+vae = VAE().load_from_checkpoint('/home/asajw/SETI/notebooks/m-0ch-recon-vae_epoch=02_elbo=260498.81.ckpt')
+trainer = pl.Trainer(gpus=1, max_epochs=20, progress_bar_refresh_rate=10, callbacks=[ckp_clbk])
+trainer.fit(vae, inputs_loader)
